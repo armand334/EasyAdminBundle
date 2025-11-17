80f6f77317ed0303ea52aae7914d95192340ba5c
Peymon Armand <peymonarmand@Peymons-MacBook-Pro.local>
2025-11-18 09:02:24 +1300

    dump batchDel and edit

--- src/Controller/AbstractCrudController.php
+++ src/Controller/AbstractCrudController.php
@@ -223,6 +223,8 @@ abstract class AbstractCrudController extends AbstractController implements Crud
             throw new InsufficientEntityPermissionException($context);
         }
 
+        dd($context->getEntity(), $context->getCrud()->getEditFormOptions(), $context);
+
         $this->container->get(FieldFactory::class)->processFields($context->getEntity(), FieldCollection::new($this->configureFields(Crud::PAGE_EDIT)), Crud::PAGE_EDIT);
         $context->getCrud()->setFieldAssets($this->getFieldAssets($context->getEntity()->getFields()));
         $this->container->get(ActionFactory::class)->processEntityActions($context->getEntity(), $context->getCrud()->getActionsConfig());
@@ -420,6 +422,8 @@ abstract class AbstractCrudController extends AbstractController implements Crud
             return $this->redirectToRoute($context->getDashboardRouteName());
         }
 
+        dd($context->getEntity(), $context->getCrud()->getEditFormOptions(), $context);
+
         /** @var EntityManagerInterface $entityManager */
         $entityManager = $this->container->get('doctrine')->getManagerForClass($batchActionDto->getEntityFqcn());
         $repository = $entityManager->getRepository($batchActionDto->getEntityFqcn());
@@ -490,7 +494,7 @@ abstract class AbstractCrudController extends AbstractController implements Crud
         // $this->container->get(ActionFactory::class)->processEntityActions($context->getEntity(), $context->getCrud()->getActionsConfig());
 
         dd($context->getEntity(), $context->getCrud()->getEditFormOptions(), $context);
-        
+
         $editForm = $this->createEditForm($context->getEntity(), $context->getCrud()->getEditFormOptions(), $context);
         $editForm->handleRequest($context->getRequest());
 
